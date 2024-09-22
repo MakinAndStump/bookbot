3712b4dc1710fd3b814acd00b52e92d3500bc592
MakinAndStump <mnimic01@gmail.com>
2024-09-21 22:23:03 -0500

    Updated the word count definition

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 def main():
 	book_path = "books/frankenstein.txt"
 	text = get_book_text(book_path)
-	count = count_words(book_path)
+	count = count_words(text)
 	print(text)
-	count(text)
+	print(f"The number of words in this book is {count}")
 
 def get_book_text(path):
 	with open(path) as f:
 		return f.read()
 
-def count_words(path):
-	with open(path) as t:
-		
-		word_count = len(t.read().split())
-		return word_count
+def count_words(text):
+	word_count = text.split()
+	return len(word_count)
 
 main()
 
