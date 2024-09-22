8676d1eb28f24c79733374c1565196911fb0418f
MakinAndStump <mnimic01@gmail.com>
2024-09-21 21:50:35 -0500

    added frankenstein printer script

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("hello world")
+def main():
+	book_path = "books/frankenstein.txt"
+	text = get_book_text(book_path)
+	print(text)
+
+def get_book_text(path):
+	with open(path) as f:
+		return f.read()
+
+
+def count_words(path)
+	
+	word_count = text.split()
+
+main()
 
